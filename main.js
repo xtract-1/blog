ee47da6cb765249d1ad91e3ef556ac34e7a0c2ae
xtract-1 <emmanuelnwakaego92@gmail.com>
2025-04-14 03:43:01 -0700

    main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+ const navItems = document.querySelector('.nav-items');
+const openNavBtn = document.querySelector('#open_nav-btn');
+const  closeNavBtn= document.querySelector('#close_nav-btn');
+
+
+const openNav = () =>{
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display='inline-block';
+
+}
+// close dropdown
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display='none';
+
+}
+
+
+
+
+
+openNavBtn.addEventListener('click', openNav);
+closeNavBtn.addEventListener('click', closeNav);
