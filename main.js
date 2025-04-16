82ffa0730cd8ea14ed73fb10ba86afdfd5d8c510
xtract-1 <emmanuelnwakaego92@gmail.com>
2025-04-16 17:40:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- const navItems = document.querySelector('.nav-items');
+const navItems = document.querySelector('.nav-items');
 const openNavBtn = document.querySelector('#open_nav-btn');
 const  closeNavBtn= document.querySelector('#close_nav-btn');
 
@@ -23,3 +23,37 @@ const closeNav = () => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+
+
+
+// showSidebarBar
+const showSidebarBar = () => {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+
+}
+// hideSidebarBar
+const hideSidebarBar = () => {
+    sidebar.style.left = '-100%';
+    showSidebarBtn.style.display = 'inline-block';
+    hideSidebarBtn.style.display = 'none';
+
+}
+
+
+
+
+
+
+
+
+
+showSidebarBtn.addEventListener('click', showSidebarBar);
+hideSidebarBtn.addEventListener('click', hideSidebarBar);
